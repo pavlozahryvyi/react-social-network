e45a3bf69b89b95e1e334835741bb76861712650
Pavel <zagryviy@gmail.com>
2020-02-13 16:07:58 +0200

    Updates

---
+++ precache-manifest.546da2a47660fcd391ea1d36c7918232.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016e573e5a8c49b1843ea7eac3625a43",
+    "url": "/react-social-network/index.html"
+  },
+  {
+    "revision": "4905f290996cf71967c8",
+    "url": "/react-social-network/static/css/main.94b3fd2b.chunk.css"
+  },
+  {
+    "revision": "1ffb64bb019df9ee2e43",
+    "url": "/react-social-network/static/js/2.e945ef97.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-social-network/static/js/2.e945ef97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4905f290996cf71967c8",
+    "url": "/react-social-network/static/js/main.ec701603.chunk.js"
+  },
+  {
+    "revision": "84f2f2b4e0e02e3ba4ca",
+    "url": "/react-social-network/static/js/runtime-main.53cd0e6c.js"
+  },
+  {
+    "revision": "b970052f8e179f0c7b2284ab9da8cb86",
+    "url": "/react-social-network/static/media/logo.b970052f.png"
+  }
+]);
