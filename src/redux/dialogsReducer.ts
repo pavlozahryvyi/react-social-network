98cc38ea10ce653bb8b973ce9585f2c16922f71e
Pavel <zagryviy@gmail.com>
2020-11-07 00:35:52 +0200

    types for dialogsReducer

--- src/redux/dialogsReducer.ts
+++ src/redux/dialogsReducer.ts
@@ -1,35 +1,48 @@
+import {initialStateType} from "./appReducer";
+
 const ADD_MESSAGE = 'dialogsReducer/ADD-MESSAGE';
 
-let initialState = {
+type DialogType = {
+    id: number
+    name: string
+}
+
+type MessageType = {
+    id: number
+    message: string
+}
+
+const initialState = {
     dialogData: [
         {id: 1, name: 'Pavel'},
         {id: 2, name: 'Andrey'},
         {id: 3, name: 'Egor'},
         {id: 4, name: 'Sasha'},
-    ],
+    ] as Array<DialogType>,
 
     messagesData: [
         {id: 1, message: 'Yo!'},
         {id: 2, message: 'How are you?'},
         {id: 3, message: 'What are you doing?'},
         {id: 4, message: 'Hi!'},
-    ]
+    ] as Array<MessageType>
 };
 
-const dialogsReducer = (state = initialState, action) => {
+export type InitialStateType = typeof initialState
+
+const dialogsReducer = (state = initialState, action: any) : InitialStateType => {
 
     switch (action.type) {
         case(ADD_MESSAGE):
 
-            let newMessage = {
+            const newMessage = {
                 id: 5,
                 message: action.newMessageText,
             };
 
             return {
                 ...state,
-                messagesData: [...state.messagesData, newMessage],
-                newMessageText: ''
+                messagesData: [...state.messagesData, newMessage]
             };
 
         default:
@@ -37,7 +50,7 @@ const dialogsReducer = (state = initialState, action) => {
     }
 };
 
-export const addMessage = (newMessageText) => ({type: ADD_MESSAGE, newMessageText});
+export const addMessage = (newMessageText: string) => ({type: ADD_MESSAGE, newMessageText});
 
 
 export default dialogsReducer;
