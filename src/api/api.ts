358267857c600c10da1f5db0659e11793b551d59
zahryvyipavlo <zagriviy2@gmail.com>
2024-02-03 23:10:27 +0200

    refactoring

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   withCredentials: true,
   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
   headers: {
-    'API-KEY': '506264c7-e08d-447a-ae53-bbc91bc9de7d'
+    'API-KEY': process.env.REACT_APP_API_KEY
   }
 });
--- src/features/api/baseQuery.ts
+++ src/features/api/baseQuery.ts
@@ -3,7 +3,7 @@ import { fetchBaseQuery } from '@reduxjs/toolkit/dist/query';
 export default fetchBaseQuery({
   baseUrl: 'https://social-network.samuraijs.com/api/1.0',
   headers: {
-    'API-KEY': '506264c7-e08d-447a-ae53-bbc91bc9de7d'
+    'API-KEY': process.env.REACT_APP_API_KEY
   },
   credentials: 'include'
 });
